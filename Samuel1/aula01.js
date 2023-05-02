040fe86477dedaa48612329f3e47501a3cf8afe2
Samuel Gomes <129182196+GomesSamuel@users.noreply.github.com>
2023-05-02 11:47:23 -0300

    Add files via upload

---
+++ Samuel1/aula01.js
@@ -0,0 +1,7 @@
+var dinheiro = 150;
+var vouAPraia = false;
+var nome = "Samuelzinho "
+var sobreNome = "Guarulhos"
+
+console.log ("Instituto " + "da " + "Oportunidade " + "Social " + nome  +  sobreNome )
+console.log (`Esse é o nome do aluno: ${nome} ${sobreNome}`)

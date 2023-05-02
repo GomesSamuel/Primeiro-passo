e105f93ab85ede02a398dafb7336e2a3b535e46f
Samuel Gomes <129182196+GomesSamuel@users.noreply.github.com>
2023-05-02 11:46:46 -0300

    Add files via upload

---
+++ Samuel/exer01.js
@@ -0,0 +1,21 @@
+var carteira = 150;
+var corinthinas = false
+var ingresso = 130;
+console.log (carteira>ingresso)
+
+
+
+
+var nome = "Samuelzinho "
+var sobreNome = "Guarulhos"
+console.log ("Gaviões" + "da " + "Fiel")
+console.log (`Esse fiel torcedor é o: ${nome} ${sobreNome}`)
+
+
+var num1 = 8;
+var num2 = 5;
+var soma = num1 + num2
+console.log (`A soma de ${num1} e ${num2} é igual à ${soma}`)
+
+
+
